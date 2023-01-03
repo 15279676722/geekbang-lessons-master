54b73d618c1dcee91bffb9c8235aad16a3be0c2a
杨强 <yangqiang@yunquna.com>
2023-01-03 20:21:54 +0800

    变更

---
+++ think-in-springboot/src/test/java/com/example/thinkinspringboot/ThinkInSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.thinkinspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThinkInSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
