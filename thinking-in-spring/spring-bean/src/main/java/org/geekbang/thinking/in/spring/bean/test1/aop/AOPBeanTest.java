5d383abe4cd56222b88453f0c97ab3d7c1897e6e
yangqiang <smallwhite1219@163.com>
2023-04-01 15:49:06 +0800

    commit

--- thinking-in-spring/spring-bean/src/main/java/org/geekbang/thinking/in/spring/bean/test1/aop/AOPBeanTest.java
+++ thinking-in-spring/spring-bean/src/main/java/org/geekbang/thinking/in/spring/bean/test1/aop/AOPBeanTest.java
@@ -1,9 +1,10 @@
 package org.geekbang.thinking.in.spring.bean.test1.aop;
 
+import org.springframework.beans.BeansException;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class AOPBeanTest {
-    public static void main(String[] args) {
+    public static void main(String[] args)  {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AnnotationConfig.class);
 
         BeanA bean = context.getBean(BeanA.class);
